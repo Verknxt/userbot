3d71cfd6d77a7fc24fe8672d7c42ad8ccae0e84b
verknxt <lawandave@gmail.com>
2020-06-19 22:07:52 +0200

    almost done! #1

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -7,7 +7,12 @@ async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    await event.edit("`pong!`")
+    await event.edit("`"
+                     "pong!"
+                     "`")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await event.edit("`pong!\n{}`".format(ms))
+    await event.edit(
+                     "`"
+                     "pong!\n{}"
+                     "`".format(ms))
