5393978545a48d4e8289ca98c43e00ae7d770898
Verknxt <lawandave@gmail.com>
2020-06-24 00:17:52 +0200

    almost done! #4

--- userbot/plugins/spotify.py
+++ userbot/plugins/spotify.py
@@ -25,22 +25,32 @@ os.system("rm -rf *.mp3")
 def bruh(name):
     
     os.system("instantmusic -q -s "+name)
-    
+
 
 @register(outgoing=True, pattern="^.spotify(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
+async def SpotifyLoader(Spotifylod):
+    if Spotifylod.fwd_from:
         return
-    link = event.pattern_match.group(1)
-    chat = "@SpotifyMusicDownloaderBot"
-    await event.edit("the song is downloading please wait...")
+    d_link = Spotifylod.pattern_match.group(1)
+    if ".com" not in d_link:
+        await Spotifylod.edit("i need a link to download the song!")
+    else:
+        await Spotifylod.edit("the song is downloading please wait...")
+    chat = "@DeezLoadBot"
     async with bot.conversation(chat) as conv:
           try:
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
-              await bot.send_message(chat, link)
-              respond = await response
+              msg_start = await conv.send_message("/start")
+              response = await conv.get_response()
+              r = await conv.get_response()
+              msg = await conv.send_message(d_link)
+              otherthings = await conv.get_response()
+              song = await conv.get_response()
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
           except YouBlockedUserError:
-              await event.reply("please unblock @deezloadbot and try again")
+              await Spotifylod.edit("please unblock @deezloadbot and try again")
               return
-          await event.delete()
-          await bot.forward_messages(event.chat_id, respond.message)
+          await Spotifylod.delete()
+          await bot.send_file(Spotifylod.chat_id, song)
+          await Spotifylod.client.delete_messages(conv.chat_id,
+                                             [msg_start.id, response.id, r.id, msg.id, song.id])
