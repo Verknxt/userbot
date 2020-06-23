686d57698cd0787b4becfbc3970fc2a3e394b4d0
Verknxt <lawandave@gmail.com>
2020-06-23 17:35:01 +0200

    almost done! #4

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 @command(pattern="^.ping")
 async def _(event):
-     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
          if event.fwd_from:
         return
     start = datetime.now()
