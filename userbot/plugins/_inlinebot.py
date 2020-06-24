12488230df038a5dbacbb91340c61525237a2b73
Verknxt <lawandave@gmail.com>
2020-06-24 16:02:06 +0200

    almost done! #4

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -13,11 +13,11 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         builder = event.builder
         result = None
         query = event.text
-        if event.query.user_id == bot.uid and query.startswith("userbot"):
+        if event.query.user_id == bot.uid and query.startswith("Userbot"):
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
-                " ",
+                "© Userbot Help",
                 text="{}\nCurrently Loaded Plugins: {}".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "please get your own userbot"
+            reply_pop_up_alert = "please get your own userbot and don't use mine"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
@@ -55,25 +55,26 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "please get your own userbot"
+            reply_pop_up_alert = "please get your own userbot and don't use mine"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
     ))
     async def on_plug_in_callback_query_handler(event):
         plugin_name = event.data_match.group(1).decode("UTF-8")
-        help_string = " "
+        help_string = ""
         try:
             for i in CMD_LIST[plugin_name]:
                 help_string += i
                 help_string += "\n"
         except:
             pass
-        if help_string is " ":
+        if help_string is "":
             reply_pop_up_alert = "{} is useless".format(plugin_name)
         else:
             reply_pop_up_alert = help_string
-        reply_pop_up_alert += " ".format(plugin_name)
+        reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
+            © Userbot".format(plugin_name)
         try:
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
         except: 
@@ -89,7 +90,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {}".format(" ", x),
+        "{} {}".format("⚡️", x ,"⚡️"),
         data="us_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
@@ -100,7 +101,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
     if len(pairs) > number_of_rows:
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
-            (custom.Button.inline("previous", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("next", data="{}_next({})".format(prefix, modulo_page)))
+            (custom.Button.inline("Previous", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("Next", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
