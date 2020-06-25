75116e3c056b39f6f5c7f4eb8d72add5862d54e9
Verknxt <lawandave@gmail.com>
2020-06-25 23:33:34 +0200

    almost done! #4

---
+++ userbot/plugins/screencapture.py
@@ -0,0 +1,56 @@
+import io
+import traceback
+from re import match
+from selenium import webdriver
+from asyncio import sleep
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from userbot.events import register
+from userbot import GOOGLE_CHROME_BIN, CHROME_DRIVER
+
+
+@register(outgoing=True, pattern="^.ss(?: |$)(.*)")
+async def capture(url):
+    """ For .ss command, capture a website's screenshot and send the photo. """
+    await url.edit("processing...")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--test-type")
+    chrome_options.binary_location = GOOGLE_CHROME_BIN
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--disable-gpu')
+    driver = driver = webdriver.Chrome(ChromeDriverManager().install())
+    input_str = url.pattern_match.group(1)
+    link_match = match(r'\bhttps?://.*\.\S+', input_str)
+    if link_match:
+        link = link_match.group()
+    else:
+        await url.edit("i need a valid link to take a screenshot")
+        return
+    driver.get(link)
+    height = driver.execute_script(
+        "return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);"
+    )
+    width = driver.execute_script(
+        "return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);"
+    )
+    driver.set_window_size(width + 500, height + 500)
+    wait_for = height / 1000
+    await url.edit(f"waiting `{int(wait_for)}`seconds for the website to load...")
+    await sleep(int(wait_for))
+    im_png = driver.get_screenshot_as_png()
+    # saves screenshot of entire page
+    driver.close()
+    message_id = url.message.id
+    if url.reply_to_msg_id:
+        message_id = url.reply_to_msg_id
+    with io.BytesIO(im_png) as out_file:
+        out_file.name = "screencapture.png"
+        await url.edit("uploading the screenshot as file...")
+        await url.client.send_file(url.chat_id,
+                                   out_file,
+                                   caption=input_str,
+                                   force_document=True,
+                                   reply_to=message_id)
