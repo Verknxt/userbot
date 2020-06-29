51ae418002ab7c3b4c8ba7d5a015d66a9585d153
verknxt <lawandave@gmail.com>
2020-06-29 19:05:15 +0200

    almost done! #5

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,11 +5,14 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 from telethon import version
+import platform
 from platform import python_version, uname
+
+userbot_python_version = platform.python_version()
+userbot_python_version_url = userbot_python_version.replace(".", "")
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
-    """ For .alive command, check if the bot is running.  """
     await alive.edit("i'm alive!\n\n"
-                                f"telethon version: `{version.__version__}`"
-                                f"\npython version: `{python_version()}`")
+                                f"telethon version: [{version.__version__}](https://pypi.org/project/Telethon/{version.__version__}/)"
+                                f"\npython version: [{python_version()}](https://www.python.org/downloads/release/python-{}/)".format(userbot_python_version_url))
