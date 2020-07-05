bd4b8851e21edf5562cd491b9f19a077ede7a12b
verknxt <lawandave@gmail.com>
2020-07-05 17:59:40 +0200

    almost done! #6

--- userbot/plugins/id.py
+++ userbot/plugins/id.py
@@ -1,15 +1,11 @@
-import asyncio
-import platform
 from telethon import events
-from telethon.tl.types import ChannelParticipantsAdmins
-from platform import uname
-from userbot import ALIVE_NAME
-from userbot.utils import admin_cmd
-from telethon import version
-from platform import python_version, uname
+from datetime import datetime
 
 
-@command(outgoing=True, pattern="^.id$")
-async def chatid(id, event):
-    chat_id = event.chat_id
-    await id.edit(f"{chat_id}")
+@command(pattern="^.ping")
+async def _(event):
+    if event.fwd_from:
+        return
+        global chat_id
+        chat_id = event.chat_id
+        await event.edit(f"{chat_id}")
