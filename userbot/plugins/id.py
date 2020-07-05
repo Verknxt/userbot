50d4e621d175993e11fe6a7fb05ec57f7409fbb8
verknxt <lawandave@gmail.com>
2020-07-05 17:07:41 +0200

    almost done! #6

---
+++ userbot/plugins/id.py
@@ -0,0 +1,17 @@
+import asyncio
+import platform
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+from telethon import version
+from platform import python_version, uname
+
+
+@command(pattern="^.id")
+async def _(event):
+    if event.fwd_from:
+        return
+        id = event.chat_id
+    await event.edit(f"`{id}`")
